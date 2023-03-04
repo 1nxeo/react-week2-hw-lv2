b29a0c5b6b22c0d7cca474a40b39aa7ac621c121
Inseo Yang <y.1nseooo@gmail.com>
2023-03-04 19:36:37 +0900

    css_최..종..아마도.. 근데 버튼 제외..

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import GlobalStyle from "../static/GlobalStyle";
+import styled from "styled-components";
 
 function Detail() {
   const navigate = useNavigate();
@@ -14,18 +15,39 @@ function Detail() {
   return (
     <>
       <GlobalStyle />
-      <h2>{foundData.id}</h2>
-      <h2>{foundData.title}</h2>
-      <p>{foundData.desc}</p>
-      <button
-        onClick={() => {
-          navigate("/");
-        }}
-      >
-        Back
-      </button>
+      <DetailWrapper>
+        <DetailContent>
+          <h2>{foundData.id}</h2>
+          <h2>{foundData.title}</h2>
+          <p>{foundData.desc}</p>
+          <button
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            Back
+          </button>
+        </DetailContent>
+      </DetailWrapper>
     </>
   );
 }
+
+const DetailWrapper = styled.div`
+  margin: auto;
+  max-width: 1200px;
+  min-width: 800px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: auto;
+`;
+
+const DetailContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+  width: 70%;
+`;
 
 export default Detail;
